a02b912d1384f29e12b0b96af2c6bfd40366e858
mayibtihel <ibtihel.may@esprit.tn>
2022-04-20 14:17:29 +0100

    Interfaces
    Reset Password Interface

--- TravediaJava/src/gui/ResetPasswordController.java
+++ TravediaJava/src/gui/ResetPasswordController.java
@@ -7,7 +7,11 @@ package gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,12 +20,25 @@ import javafx.fxml.Initializable;
  */
 public class ResetPasswordController implements Initializable {
 
+    @FXML
+    private TextField email;
+    @FXML
+    private Button retourlogin;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    @FXML
+    private void envoyer(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancel(ActionEvent event) {
     }
 
 }
